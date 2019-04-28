c0d770a802feda676ce1a174f7ddb7a8d33bae0d
henryuniuni <ak47m245@gmail.com>
2019-04-28 23:25:42 +0800

    Add files via upload

--- evil-extension/page.js
+++ evil-extension/page.js
@@ -12,9 +12,32 @@ const MATCH_LIST = {
 
 function transformTextNodes(node) {
   // TODO(you): Implement this function! See HW spec for details.
+  if(node.nodeType === Node.TEXT_NODE){
+    const words = node.textContent.trim().split(' ');
+    for(var i=0; i<words.length; i++){
+      if(words[i] == ' ' || words[i] == '\t' || words[i] == '\n' || words[i] == ''){
+        continue;
+      }
+      else{
+        for(var m in MATCH_LIST){
+          if(words[i].match(m)){
+            //console.log("found");
+            words[i] = words[i].replace(m, MATCH_LIST[m]);
+            //console.log(words[i]);
+            break;
+          }
+        }
+      }
+    }
+    node.textContent = words.join(" ");
+  }
+  for (const child of node.childNodes) {
+    transformTextNodes(child);
+  }
 }
 
 transformTextNodes(document.body);
 
 // Log statement to test that the extension loaded properly.
 console.log('Evil extension loaded!');
+console.log('Extension updated');
